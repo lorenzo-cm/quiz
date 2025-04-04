8272f86d9da4a2529f722ae800e50cd4ca1507cd
Lorenzo Magalhaes <lorenzocarneirobr@gmail.com>
2025-04-04 15:57:04 -0300

    Commit 2: Creating 10 unit tests.

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 import pytest
-from model import Question
-
+from model import Question, Choice
 
 def test_create_question():
     question = Question(title='q1')
@@ -34,3 +33,111 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+    
+
+# ---
+
+#1
+def test_add_multiple_choices():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    
+    assert len(question.choices) == 2
+    assert question.choices[0].text == 'a'
+    assert question.choices[1].text == 'b'
+    
+    assert question.choices[0].is_correct == False
+    assert question.choices[1].is_correct == True
+
+#2
+def test_remove_choice_by_id():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    
+    question.remove_choice_by_id(1)
+    
+    assert len(question.choices) == 1
+    assert question.choices[0].text == 'b'
+
+#3
+def test_remove_all_choices():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    
+    question.remove_all_choices()
+    
+    assert len(question.choices) == 0
+
+#4
+def test_select_correct_choices():
+    question = Question(title='q1', max_selections=2)
+    choice1 = question.add_choice('a', True)
+    choice2 = question.add_choice('b', False)
+    choice3 = question.add_choice('c', True)
+    
+    selected = question.select_choices([choice1.id, choice3.id])
+    
+    assert selected == [choice1.id, choice3.id]
+
+#5
+def test_set_correct_choices():
+    question = Question(title='q1', max_selections=2)
+    choice1 = question.add_choice('a', True)
+    choice2 = question.add_choice('b', False)
+    choice3 = question.add_choice('c', False)
+    
+    question.set_correct_choices([choice1.id, choice3.id])
+    
+    assert [True, False, True] == [choice1.is_correct, choice2.is_correct, choice3.is_correct]
+
+#6
+def test_get_invalid_id():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    
+    with pytest.raises(Exception):
+        question._choice_by_id('invalid_id')
+     
+#7
+def test_remove_invalid_id():
+    question = Question(title='q1')
+    question.add_choice('a', False)
+    
+    with pytest.raises(Exception):
+        question.remove_choice_by_id('2')
+        
+#8
+def test_error_max_selections():
+    question = Question(title='q1', max_selections=2)
+    choice1 = question.add_choice('a', True)
+    choice2 = question.add_choice('b', False)
+    choice3 = question.add_choice('c', False)
+    
+    with pytest.raises(Exception) as e:
+        question.select_choices([choice1.id, choice2.id, choice3.id])
+    
+    assert str(e.value) == 'Cannot select more than 2 choices'
+    
+#9
+def test_error_long_choice_text():
+    with pytest.raises(Exception) as e:
+        Choice(0, "abc" * 300)
+    
+    assert str(e.value) == 'Text cannot be longer than 100 characters'
+    
+#10
+def test_error_empty_text():
+    with pytest.raises(Exception) as e:
+        Choice(0, text="")
+    
+    assert str(e.value) == 'Text cannot be empty'
+    
+#11
+def test_error_too_much_points_in_question():
+    with pytest.raises(Exception) as e:
+        Question(title="q1", points=101)
+    
+    assert str(e.value) == 'Points must be between 1 and 100'
